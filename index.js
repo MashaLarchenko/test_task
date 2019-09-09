45392a4e30f1418c4361ba3c0639d98d00823388
Maryia Larchanka <mashalarchenko16@gmail.com>
2019-09-09 17:11:57 +0300

    fix: some bag

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ let encriptMessage = document.querySelector('#crypt_message');
 
 let answerMessage = document.querySelector('#answer-area');
 
-function  edwardDecrypt() {
- let encriptValue = [...encriptMessage.value];
-result = encriptValue.filter((item, index, arr) => {
-    return (item!==arr[index-1] && item!==arr[index+1]);
+function edwardDecrypt() {
+    let encriptValue = [...encriptMessage.value];
+    let result = encriptValue.filter((item, index, arr) => {
+        return (item !== arr[index - 1] && item !== arr[index + 1]);
 
-  });
- answerMessage.value = result.join('');
+    });
+    answerMessage.value = result.join('');
 }
 
-button.addEventListener('click', edwardDecrypt) ;
+button.addEventListener('click', edwardDecrypt);
